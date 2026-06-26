662e149ed0f8ccb5a8fef77ec151af01c116a666
wb199706047072 <1125565732@qq.com>
2026-06-26 11:31:49 +0800

    feat(home): 添加车辆管理功能入口
    - 更新页面描述，移除业务子页面相关内容
    - 添加车辆管理菜单定位器
    - 实现跳转到车辆列表页面的功能方法
    - 集成车辆管理页面的导航逻辑

--- projects/clue/pages/home_page.py
+++ projects/clue/pages/home_page.py
@@ -3,7 +3,7 @@
 # @Author  : 会飞的🐟
 # @File    : home_page.py
 # @Software: PyCharm
-# @Desc    : 登录成功后的入口页（/welcome），负责派发到各业务子页面
+# @Desc    : 登录成功后的入口页
 
 import allure
 from utils.base_utils.base_page import BasePage
@@ -24,6 +24,7 @@ class HomePage(BasePage):
     locator_welcome_tip = "xpath=//*[@id='root']/div/div[2]/div[2]/header[2]/div/div[3]/div/div/div/span/div/div[2]/div/span"
     # 左侧菜单
     locator_menu_account_management = "text=账号管理"
+    locator_menu_vehicle_management = "text=车辆管理"
 
     @allure.step("校验已成功进入首页 /welcome")
     def assert_on_home(self):
@@ -57,3 +58,16 @@ class HomePage(BasePage):
         from pages.data.data_page import DataPage
 
         return DataPage(self.page)
+
+    @allure.step("从首页跳转到【车辆管理】-【车辆列表】")
+    def goto_vehicle_list(self):
+        """
+        点击左侧菜单【车辆管理】并进入【车辆列表】页面。
+
+        :return: VehicleListPage 实例
+        """
+        from pages.vehicle.vehicle_list_page import VehicleListPage
+
+        vehicle_list_page = VehicleListPage(self.page)
+        vehicle_list_page.click_menu_vehicle_management().click_vehicle_list()
+        return vehicle_list_page
